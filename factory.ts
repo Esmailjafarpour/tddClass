2031dbe0bcf878702e59813974c4454f66959aba
nader <esmailjafarpour29@gmail.com>
2024-12-31 18:06:43 +0330

    task

---
+++ factory.ts
@@ -0,0 +1,73 @@
+
+class lineCreate {
+    public createMethod ():void{
+
+    }
+}
+
+class bic implements lineCreate{
+    createMethod (){
+        console.log("create bic")
+    }
+}
+
+class khodroo implements lineCreate{
+    createMethod (){
+        console.log("create car")
+    }
+}
+
+// class ship implements lineCreate{
+//     createMethod (){
+//
+//     }
+// }
+
+abstract class naderFactory {
+    order:lineCreate
+    abstract getOrder():void
+    abstract orderDelivery():void
+
+}
+
+class bicycle extends naderFactory{
+
+    getOrder():lineCreate{
+        return new bic()
+    }
+
+    orderDelivery(): void{
+        this.order = this.getOrder()
+        this.order.createMethod()
+    }
+
+}
+
+
+class samand extends naderFactory{
+
+    getOrder():lineCreate{
+        return new khodroo()
+    }
+
+    orderDelivery():void{
+        this.order = this.getOrder()
+        this.order.createMethod()
+    }
+}
+
+const createSaman = new samand()
+createSaman.orderDelivery()
+
+const createBicycle = new bicycle()
+createBicycle.orderDelivery()
+
+
+
+
+
+
+
+
+
+
